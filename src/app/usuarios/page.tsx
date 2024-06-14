fdbc1724bcd8a4c06dd786065701fd44a7ce2957
Bigodrigo <roesbo1@hotmail.com>
2024-06-14 09:55:02 -0300

    List effects and layout updates

--- src/app/usuarios/page.tsx
+++ src/app/usuarios/page.tsx
@@ -6,12 +6,12 @@ export default async function Usuarios() {
 
     return (
         <main className="flex flex-col items-center justify-start py-4 px-4 text-pri">
-            <h1 className='text-lg mb-2 md:text-3xl'>Usuários</h1>
-            <ul className='border-t border-b border-black py-4'>
+            <h1 className='text-xl mb-2 md:text-3xl text-pri font-semibold'>Usuários</h1>
+            <ul className='border-t border-b border-black py-4 md:grid md:grid-cols-4 gap-4'>
                 {user.map((user) => (
-                    <li key={user.id} className='flex items-center justify-between px-5'>
+                    <li key={user.id} className='flex items-center justify-center px-5 my-2 rounded-lg  bg-sec hover:scale-110 hover:animate-pulse'>
                         <Link href={`usuarios/${user.id}`}>
-                            <p className='text-pri'>{user.nome}</p>
+                            <p className='text-white text-center text-sm md:text-base content-center py-1 md:h-12'>{user.nome}</p>
                         </Link>
                     </li>
                 ))}
