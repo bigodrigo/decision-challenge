ec89b6d22429d05581ac62e0cecdcea3d4a7aa2d
Bigodrigo <roesbo1@hotmail.com>
2024-06-15 20:14:48 -0300

    Correction for deploy

--- src/lib/validationSchema.ts
+++ src/lib/validationSchema.ts
@@ -1,20 +1,36 @@
-import * as yup from 'yup';
+// src/lib/validationSchema.ts
+
+import * as Yup from 'yup';
 import { UserFormValues } from './interfaces';
 
-export const validationSchema = yup.object().shape({
-    nome: yup.string().min(10, 'Nome deve ter no mínimo 10 caracteres').max(100, 'Nome deve ter no máximo 100 caracteres').required('Nome é obrigatório'),
-    senha: yup.string().min(10, 'Senha deve ter no mínimo 10 caracteres').max(30, 'Senha deve ter no máximo 30 caracteres').required('Senha é obrigatória'),
-    dataNascimento: yup.date().max(new Date(), 'Data de nascimento não pode ser superior a hoje').required('Data de nascimento é obrigatória'),
-    nomeMae: yup.string().min(10, 'Nome da mãe deve ter no mínimo 10 caracteres').max(100, 'Nome da mãe deve ter no máximo 100 caracteres').required('Nome da mãe é obrigatório'),
+const validationSchema = Yup.object().shape({
+    nome: Yup.string()
+        .min(10, 'Nome deve ter pelo menos 10 caracteres')
+        .max(100, 'Nome não pode ter mais de 100 caracteres')
+        .required('Nome é obrigatório'),
+    senha: Yup.string()
+        .min(10, 'Senha deve ter pelo menos 10 caracteres')
+        .max(30, 'Senha não pode ter mais de 30 caracteres')
+        .required('Senha é obrigatória'),
+    dataNascimento: Yup.date()
+        .max(new Date(), 'Data de nascimento não pode ser no futuro')
+        .required('Data de nascimento é obrigatória'),
+    nomeMae: Yup.string()
+        .min(10, 'Nome da mãe deve ter pelo menos 10 caracteres')
+        .max(100, 'Nome da mãe não pode ter mais de 100 caracteres')
+        .required('Nome da mãe é obrigatório'),
 });
 
-export const validate = (values: UserFormValues) => {
+export const validate = async (values: UserFormValues) => {
     try {
-        validationSchema.validateSync(values, { abortEarly: false });
+        await validationSchema.validate(values, { abortEarly: false });
         return {};
     } catch (error) {
-        return error.inner.reduce((errors: { [key: string]: string }, err: { path: string; message: string }) => {
-            errors[err.path] = err.message;
+        const validationError = error as Yup.ValidationError; // Asserção de tipo
+        return validationError.inner.reduce((errors: { [key: string]: string }, err) => {
+            if (err.path) {  // Verifique se err.path não é indefinido
+                errors[err.path] = err.message;
+            }
             return errors;
         }, {});
     }
